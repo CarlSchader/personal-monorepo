04c21200ccf9dc767e1714ca4a4ffbfc21a33ae6
Carl Schader <carlschader@gmail.com>
2025-04-05 03:51:01 -0400

    todos

--- src/remind/remind.py
+++ src/remind/remind.py
@@ -1,4 +1,5 @@
 import asyncio
+import aiohttp
 import os
 import argparse
 
@@ -7,6 +8,14 @@ import telegram
 BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
 
 assert len(BOT_TOKEN) > 0, "BOT_TOKEN not set"
+
+
+async def generate_markdown_reminder_string(markdown_url: str) -> str:
+    async with aiohttp.ClientSession() as ses:
+        async with ses.get(markdown_url) as res:
+            if res.status < 200 or res.status >= 300:
+                raise Exception(f"error retrieving : {markdown_url}")
+            return await res.text() 
 
 
 async def send_telegram(bot: telegram.Bot, chat_id: str, message: str):
@@ -24,7 +33,6 @@ async def execute_async(message: str):
             print(bot_info)
             exit(1)
         
-
         chat_ids = [
             update.message.from_user.id for update in updates 
             if update.message is not None and update.message.from_user is not None # Not sure if optional chaining is possible here instead
