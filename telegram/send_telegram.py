73c686d21c1e46c5f80f4ac2dc564f54686560b3
Carl Schader <carlschader@gmail.com>
2025-04-04 21:38:31 -0400

    updates

--- telegram/send_telegram.py
+++ telegram/send_telegram.py
@@ -4,25 +4,24 @@ import os
 
 async def main():
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    BOT_CHAT_ID = os.getenv("BOT_CHAT_ID")
 
     assert BOT_TOKEN is not None
 
     bot = telegram.Bot(BOT_TOKEN)
     async with bot:
-        # set BOT_CHAT_ID if not set and alert the user
-        if BOT_CHAT_ID is None:
-            bot_info = await bot.get_me()
-            BOT_CHAT_ID = bot_info.id
-            print(f"export BOT_CHAT_ID=\"{BOT_CHAT_ID}\"")
-
-        # send message
-        bot.send_message(text="test", chat_id=BOT_CHAT_ID)
+        updates = await bot.get_updates()
+        print(updates)
+        
+        chat_ids = [update.message.from_user.id for update in updates]
+        
+        for chat_id in chat_ids:
+            # send message
+            bot.send_message(text="test", chat_id=BOT_CHAT_ID)
 
 
 if __name__ == '__main__':
     import argparse
 
-    
+     
 
     asyncio.run(main())
