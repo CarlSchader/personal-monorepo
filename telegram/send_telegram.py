c9a27e95d26f76badf3b408d31807adb35948dd4
Carl Schader <carlschader@gmail.com>
2025-04-04 20:40:09 -0400

    telegram

--- telegram/send_telegram.py
+++ telegram/send_telegram.py
@@ -1,12 +1,28 @@
 import asyncio
 import telegram
-
+import os
 
 async def main():
-    bot = telegram.Bot("TOKEN")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    BOT_CHAT_ID = os.getenv("BOT_CHAT_ID")
+
+    assert BOT_TOKEN is not None
+
+    bot = telegram.Bot(BOT_TOKEN)
     async with bot:
-        print(await bot.get_me())
+        # set BOT_CHAT_ID if not set and alert the user
+        if BOT_CHAT_ID is None:
+            bot_info = await bot.get_me()
+            BOT_CHAT_ID = bot_info.id
+            print(f"export BOT_CHAT_ID=\"{BOT_CHAT_ID}\"")
+
+        # send message
+        bot.send_message(text="test", chat_id=BOT_CHAT_ID)
 
 
 if __name__ == '__main__':
+    import argparse
+
+    
+
     asyncio.run(main())
