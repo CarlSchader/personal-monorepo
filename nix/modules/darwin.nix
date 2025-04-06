79f0f5631193908eff9609d8c198efad8d8bef07
Carl Schader <carl.schader@saronic.com>
2025-04-06 12:46:51 -0500

    motd-fix

--- nix/modules/darwin.nix
+++ nix/modules/darwin.nix
@@ -23,7 +23,7 @@
     # The platform the configuration will be used on.
     nixpkgs.hostPlatform = "aarch64-darwin";
 
-    users.motd = 
+    users.users.motd = 
     ''
                                 ____                    __         
        /'\_/`\                 /\  _`\                 /\ \        
