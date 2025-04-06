8f3f009e683cce27d19b32e66d8a3983a87cf8b1
Carl Schader <carlschader@gmail.com>
2025-04-06 16:32:27 -0400

    redo-timing

--- nix/modules/personal-services.nix
+++ nix/modules/personal-services.nix
@@ -4,15 +4,21 @@
     systemd.timers."remind-timer" = {
       wantedBy = [ "timers.target" ];
       timerConfig = {
-        OnBootSec = "5m";
+        OnBootSec = "1m";
         OnUnitActiveSec = "5m";
+        OnCalendar = [
+          "*-*-* 08:00:00"
+          "*-*-* 12:00:00"
+          "*-*-* 18:00:00"
+        ];
+        Persistent = true;
         Unit = "remind-timer.service";
       };
     };
 
     systemd.services."remind-timer" = {
       enable = true; 
-      script = "${personal-monorepo}/bin/remind -b=/home/carl/secrets/personal-monorepo-bot-token";
+      script = "${personal-monorepo}/bin/remind -b=/home/carl/secrets/personal-monorepo-bot-token"; # change this to somewhere in /etc in the future
       serviceConfig = {
         Type = "oneshot";
         User = "root";
