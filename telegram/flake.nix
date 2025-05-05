04fda38fa939308bf62b7d7191b4ece94a27730a
Carl Schader <carlschader@gmail.com>
2025-05-04 23:10:14 -0400

    systemd

--- telegram/flake.nix
+++ telegram/flake.nix
@@ -36,6 +36,11 @@
       type = "app";
       program = "${packages.default}/bin/server";
     };
+
+    apps.remind = {
+      type = "app";
+      program = "${packages.default}/bin/remind";
+    };
     
     apps.register-webhook = {
       type = "app";
@@ -46,5 +51,58 @@
       type = "app";
       program = "${packages.default}/bin/unregister-webhook";
     };
+
+    nixosModule.remind = { config, lib, pkgs, ... }:
+    with lib;
+    let
+      cfg = config.services.telegram-remind;
+    in 
+    {
+      options.services.telegram-remind = {
+        enable = mkEnableOption "enable the telegram-remind.timer service";
+
+        bot-token = mkOption {
+          type = types.string;
+          default = "";
+          description = "telegram bot token to use. REQUIRED";
+        };
+
+        OnCalendar = mkOption {
+          description = "systemd timer calendar rules for when the service activates";
+          default = [
+            "*-*-* 08:00:00"
+            "*-*-* 14:00:00"
+            "*-*-* 20:00:00"
+          ];
+        };
+
+        OnBootSec = mkOption {
+          description = "systemd timer OnBootSec rule for how long after booting to activate the service";
+          default = "1m";
+        };
+      };
+
+      config = mkIf cfg.enable {
+        systemd.timers."telegram-remind" = {
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnBootSec = cfg.OnBootSec;
+            OnCalendar = cfg.OnCalendar;
+            Persistent = true;
+            Unit = "telegram-remind.timer";
+          };
+        };
+
+        systemd.services."telegram-remind" = {
+          enable = true; 
+          script = "${packages.default}/bin/remind -b ${cfg.bot-token}"; # change this to somewhere in /etc in the future
+          serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+          };
+        };
+      };
+    };
+     
   });
 }
