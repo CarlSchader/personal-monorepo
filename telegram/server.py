7f0f7113e3ff643924361b2a7ef5d3bc22249d61
Carl Schader <carlschader@gmail.com>
2025-05-06 00:21:47 -0400

    working nginx

--- telegram/server.py
+++ telegram/server.py
@@ -4,12 +4,12 @@ import uvicorn
 app = FastAPI()
 
 @app.get("/")
+@app.get("/health")
 async def root():
     return {"message": "healthy"}
 
 @app.post("/webhook")
 async def webhook(request: Request):
-    logger.info(request)
     return {"message": "Webhook received"}
 
 @app.post("/hello-webhook")
@@ -18,7 +18,7 @@ async def hello_webhook(request: Request):
     return {"message": "Hello webhook received"}
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=88)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     main()
