ad3cada5e74001e19e62c0734f4e645037ca0f13
Carl Schader <carlschader@gmail.com>
2025-05-09 05:19:38 -0400

    boto3 parsing of creds

--- telegram/server.py
+++ telegram/server.py
@@ -8,33 +8,77 @@ import aiohttp
 import telegram
 from fastapi import FastAPI, Request, Response, status
 import uvicorn
+import boto3
+
+### SETUP CONSTANTS AND GLOBALS ###
 
 logger = logging.getLogger('uvicorn.info')
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
-FALLBACK_TOKEN_FILE_PATH = "/etc/personal-monorepo/bot-token"
-FALLBACK_SECRET_FILE_PATH = "/etc/personal-monorepo/webhook-secret"
-REPO_FILES_ROOT = "https://raw.githubusercontent.com/CarlSchader/personal-monorepo/refs/heads/main/"
+REPO_FILES_ROOT: str = "https://raw.githubusercontent.com/CarlSchader/personal-monorepo/refs/heads/main/"
 
 PORT: int = int(os.getenv("PORT", "8080"))
+
+
+## telegram bot token
+
+FALLBACK_TOKEN_FILE_PATH: str = "/etc/personal-monorepo/bot-token"
 
 BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
 if BOT_TOKEN == "" and os.path.exists(FALLBACK_TOKEN_FILE_PATH):
     with open(FALLBACK_TOKEN_FILE_PATH, 'r') as f:
-        BOT_TOKEN = f.read()
+        BOT_TOKEN = f.read().strip()
 BOT_TOKEN = BOT_TOKEN.strip()
 
 assert len(BOT_TOKEN) > 0, "bot token is empty"
 
+bot = telegram.Bot(BOT_TOKEN)
+
+
+## telegram webhook secret
+
+FALLBACK_SECRET_FILE_PATH: str = "/etc/personal-monorepo/webhook-secret"
+
 WEBHOOK_SECRET: str = os.getenv("WEBHOOK_SECRET", "")
 if WEBHOOK_SECRET == "" and os.path.exists(FALLBACK_TOKEN_FILE_PATH):
     with open(FALLBACK_SECRET_FILE_PATH, 'r') as f:
-        WEBHOOK_SECRET = f.read()
+        WEBHOOK_SECRET = f.read().strip()
 WEBHOOK_SECRET = WEBHOOK_SECRET.strip()
 
-assert len(WEBHOOK_SECRET) > 0, "bot token is empty"
+assert len(WEBHOOK_SECRET) > 0, "webhook secret is empty"
 
-bot = telegram.Bot(BOT_TOKEN)
+
+## object storage
+
+OBJECT_STORAGE_ENDPOINT: str = "https://8ffc049de8a865c3922d3097c069a717.r2.cloudflarestorage.com"
+FALLBACK_ACCESS_KEY_ID_FILE_PATH: str = "/etc/personal-monorepo/access-key-id"
+FALLBACK_SECRET_ACCESS_KEY_PATH: str = "/etc/personal-monorepo/secret-access-key"
+
+ACCESS_KEY_ID: str = os.getenv("ACCESS_KEY_ID", "")
+if ACCESS_KEY_ID == "" and os.path.exists(FALLBACK_ACCESS_KEY_ID_FILE_PATH):
+    with open(FALLBACK_ACCESS_KEY_ID_FILE_PATH, 'r') as f:
+        ACCESS_KEY_ID = f.read().strip()
+ACCESS_KEY_ID = ACCESS_KEY_ID.strip()
+
+assert len(ACCESS_KEY_ID) > 0, "access-key-id is empty"
+
+SECRET_ACCESS_KEY: str = os.getenv("SECRET_ACCESS_KEY", "")
+if SECRET_ACCESS_KEY == "" and os.path.exists(FALLBACK_SECRET_ACCESS_KEY_PATH):
+    with open(FALLBACK_SECRET_ACCESS_KEY_PATH, 'r') as f:
+        SECRET_ACCESS_KEY = f.read().strip()
+SECRET_ACCESS_KEY = SECRET_ACCESS_KEY.strip()
+
+assert len(SECRET_ACCESS_KEY) > 0, "secret-access-key is empty"
+
+s3_client = boto3.client(
+    's3',
+    endpoint_url=OBJECT_STORAGE_ENDPOINT,
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=SECRET_ACCESS_KEY,
+)
+
+
+### END SETUP ###
 
 async def fetch_repo_file(repo_sub_path: str) -> str:
     aiohttp_connector = aiohttp.TCPConnector(ssl=ssl_context)
