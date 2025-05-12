8c73a4dd6ccdc93d60a5ecfe7ba53f846d05f449
carlschader <carlschader@gmail.com>
2025-05-11 21:21:57 -0500

    works

--- repo-utils/default.nix
+++ repo-utils/default.nix
@@ -20,30 +20,15 @@ rec {
     }
   );
 
-  # packages.commit-file = pkgs.writeShellApplication {
-  #   name = "commit-file";
-  #   runtimeInputs = [ packages.repo-utils ];
-  #   text = "${packages.repo-utils}/bin/commit-file \"$@\"";
-  # };
-
   apps.commit-file = {
     type = "app";
     program = "${packages.commit-file}/bin/commit-file";
   };
 
-  packages.commit-secret-file = pkgs.stdenv.mkDerivation {
-    name = "commit-secret-file";
-    src = ./commit-secret-file.sh;
-    dontUnpack = true;
-    nativeBuildInputs = [ packages.repo-utils pkgs.gzip pkgs.gnutar pkgs.age pkgs.sops ];
-    propagatedNativeBuildInputs = [ packages.repo-utils pkgs.gzip pkgs.gnutar pkgs.age pkgs.sops ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/commit-secret-file
-      chmod +x $out/bin/commit-secret-file
-    '';
-
-  };
+  packages.commit-secret-file = pkgs.writeShellScriptBin "commit-secret-file" ''
+    export PATH="${packages.repo-utils}/bin:${pkgs.gzip}/bin:${pkgs.gnutar}/bin:${pkgs.age}/bin:${pkgs.sops}/bin:$PATH"
+    ${builtins.readFile ./commit-secret-file.sh}
+  '';
 
   packages.encrypt-store = pkgs.stdenv.mkDerivation {
     name = "encrypt-store";
