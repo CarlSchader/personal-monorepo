a46f7d98f78ca28739814be4ebf442003959e4cd
carlschader <carlschader@gmail.com>
2025-07-04 17:48:07 -0400

    javs keys plural

--- nix/keys.nix
+++ nix/keys.nix
@@ -9,7 +9,9 @@ rec {
   ];
 
   jav = [
-    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIL6B2JPmPprjUZuGiS71/XOOfrlJPH5oyfgtaSApQEvZAAAABHNzaDo="
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICeGCbI6tGjiCnb07I6LYWIe2Ig+fW5OukpMsPjwyaQ8"
+    "no-touch-required sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIL6B2JPmPprjUZuGiS71/XOOfrlJPH5oyfgtaSApQEvZAAAABHNzaDo="
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvZz4gz+5WGHH7zDI5hW740UKbXEQWNB6IakhyMECCI"
   ];
 
   saronic = [];
