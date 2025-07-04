04d4aa8439bab80bfae4334b4095832e7d8f4b92
carlschader <carlschader@gmail.com>
2025-07-04 17:21:14 -0400

    javs key

--- nix/keys.nix
+++ nix/keys.nix
@@ -8,7 +8,9 @@ rec {
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE8N1WCZEQv43tuIvndSbtSPa3uYxFUfGh6LN0BFbnyt connorjones@MacBookPro" # connor
   ];
 
-  jav = [];
+  jav = [
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIL6B2JPmPprjUZuGiS71/XOOfrlJPH5oyfgtaSApQEvZAAAABHNzaDo="
+  ];
 
   saronic = [];
 
