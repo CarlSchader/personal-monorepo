91b0e5967136f890c8072d99ec34fc6467538249
Carl Schader <carlschader@gmail.com>
2025-07-08 03:27:07 -0500

    completely rm bitcoin-carl

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,9 +1,9 @@
-{ bitcoin-carl, flake-utils, ... }:
+{ flake-utils, ... }:
 flake-utils.lib.eachDefaultSystem (system: 
   {
-    overlays.bitcoin-carl = final: prev: {
-      bitcoin-carl = bitcoin-carl.packages.${system}.default; 
-    };
+    # overlays.bitcoin-carl = final: prev: {
+    #   bitcoin-carl = bitcoin-carl.packages.${system}.default; 
+    # };
 
     overlays.darwin-packages = final: prev: {
       binutils = prev.darwin.binutils;
