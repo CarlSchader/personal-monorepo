d4318897e143c01745e95fdee15f7650409fba2a
carlschader <carlschader@gmail.com>
2025-07-13 18:16:16 -0400

    added yubikey public keys

--- nix/keys.nix
+++ nix/keys.nix
@@ -2,6 +2,8 @@ rec {
   carl = [
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIN/sQiWl/z33QQMN70MenMKDj3enVgpEVoFVus+PaHWNAAAABHNzaDo= yubikey-carl" # yubikey personal
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMIssaCueh863XJ1p8wVWNScHOehySTPmrZPjyK9PDJAAAAABHNzaDo= carls-yubikey-2"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID5SFmX6iIuILf/Xljssm1C8QgEv0jUQW2aoEh1h6WRHAAAABHNzaDo= yubikey3"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHuG0LK/0zJ0aShQ9o83SJX+q/N3Cm+QvIW+XVHDPrPLAAAABHNzaDo= yubikey4"
   ];
   
   connor = [ 
