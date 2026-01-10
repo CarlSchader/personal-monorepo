db0810debdbb7cd8c14f279b82b64aea9e34a9b5
carlschader <carlschader@gmail.com>
2026-01-09 22:41:43 -0500

    added gnome and bluetooth modules (not used)

--- nix/nixos-modules/all-systems/default.nix
+++ nix/nixos-modules/all-systems/default.nix
@@ -1,9 +1,11 @@
 { flake-utils, ... }@inputs:
 flake-utils.lib.meld inputs [
+  ./bluetooth.nix
   ./dockerd.nix
   ./experimental-featurs.nix
   ./git-server.nix
   ./git-shared-server.nix
+  ./gnome.nix
   ./motd.nix
   ./nginx-reverse-proxy.nix
   ./nix-ld.nix
--- nix/nixos-modules/x86_64-linux/users.nix
+++ nix/nixos-modules/x86_64-linux/users.nix
@@ -19,6 +19,7 @@ in
           "networkmanager"
           "wheel"
         ];
+        hashedPassword = "$6$oUx4cxq3vxiY2Czo$RdM1s7eLdnNgFV9F4UAOCFiZrkiIyW6MuIcXveRqYFmK5GrzVmvwwfkGhMNPgKk3wwu.Ybhh.//JJBPIEj/2K0";
         packages = self.common.${system}.user-packages;
         openssh.authorizedKeys.keys = keys.carl;
       };
